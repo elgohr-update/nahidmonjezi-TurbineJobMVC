cc10755e7174c7bdf3ed2d4e99565bb5bdef6603
Mohammad Mehrnia <qermezkon@gmail.com>
2020-01-01 19:28:29 +0330

    Improve UI (Use Font Awesome Pro)

--- TurbineJobMVC/Models/EntitiesConfigure/TahvilFormsConfigure.cs
+++ TurbineJobMVC/Models/EntitiesConfigure/TahvilFormsConfigure.cs
@@ -7,12 +7,12 @@ using TurbineJobMVC.Models.Entites;
 
 namespace TurbineJobMVC.Models.EntitiesConfigure
 {
-    public class TahvilFormsConfigure:BaseViewConfigure<TahvilForms>
+    public class TahvilFormsConfigure : BaseViewConfigure<TahvilForms>
     {
         public override void Configure(EntityTypeBuilder<TahvilForms> builder)
         {
             base.Configure(builder);
-            builder.HasQueryFilter(q => q.TypeTahvil != 4);
+            builder.HasQueryFilter(q => q.TypeTahvil != 4 & q.AmvalNo != 0);
         }
     }
 }
