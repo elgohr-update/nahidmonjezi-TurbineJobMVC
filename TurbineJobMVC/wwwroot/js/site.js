6cdd0223db1572c1a1f3ae7e9a797e3d0e5b7c3d
Mohammad Mehrnia <qermezkon@gmail.com>
2020-02-09 13:03:48 +0330

    Improve toastr settings

--- TurbineJobMVC/wwwroot/js/site.js
+++ TurbineJobMVC/wwwroot/js/site.js
@@ -1,22 +1,21 @@
-﻿toastr.options = {
-    "closeButton": false,
-    "debug": false,
-    "newestOnTop": false,
-    "progressBar": true,
-    "positionClass": "toast-top-left",
-    "preventDuplicates": false,
-    "onclick": null,
-    "showDuration": "300",
-    "hideDuration": "1000",
-    "timeOut": "5000",
-    "extendedTimeOut": "1000",
-    "showEasing": "swing",
-    "hideEasing": "linear",
-    "showMethod": "fadeIn",
-    "hideMethod": "fadeOut"
-};
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-left",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
     $('[data-toggle="tooltip"]').tooltip();
     $('#frmAddWorkOrder').bind('invalid-form.validate', function (form, validator) {
         showErrorMessagesOnValidate(validator.errorList);
